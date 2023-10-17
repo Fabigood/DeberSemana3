b4ed3d6709c1d7e2ed5f2f1f0eb86d73174ccad6
Fabigood <fabio.gonzalez.fiallos@udla.edu.ec>
2023-10-16 23:02:20 -0500

    final

--- 38ejer.c
+++ 38ejer.c
@@ -7,10 +7,14 @@ int main() {
 
 
     printf("Tabla de multiplicar del %d:\n", num);
-    
+    if (num >= 0) {
     for (int i = 1; i <= 10; i++) {
     printf("%d x %d = %d\n", num, i, num * i);   
-       
+            }
+    }else{
+        for (int i = 1; i <= 10; i++) {
+            printf("(-%d) x %d = %d\n", -num, i, -num * i);
+        }
     }
 
      return 0;
