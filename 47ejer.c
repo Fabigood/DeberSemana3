b9b6bffec1f930367c7a1f4596b1430e3f730c43
Fabigood <fabio.gonzalez.fiallos@udla.edu.ec>
2023-10-16 23:12:43 -0500

    agrego el for

--- 47ejer.c
+++ 47ejer.c
@@ -6,7 +6,13 @@ int main() {
     int suma = 0;
      printf("Introduce un numero entero: ");
      scanf("%d", &num);
-
+        if (num < 1) {
+        printf("El número debe ser mayor o igual a 1.\n");
+    } else {
+        for (int i = 1; i <= num; i++) {
+            fact *= i;
+            suma += fact;
+        }
 
 
     return 0;
