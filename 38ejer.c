3c10f3a0992aa626521e1fa5fbca6d8457b1b318
Fabigood <fabio.gonzalez.fiallos@udla.edu.ec>
2023-10-16 22:54:58 -0500

    imprimo todo y mulitplic

--- 38ejer.c
+++ 38ejer.c
@@ -9,7 +9,7 @@ int main() {
     printf("Tabla de multiplicar del %d:\n", num);
     
     for (int i = 1; i <= 10; i++) {
-        
+    printf("%d x %d = %d\n", num, i, num * i);   
        
     }
 
