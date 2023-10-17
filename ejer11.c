ffd79d009bbfdb81968784f30cfff5c2ab6f8026
Fabigood <fabio.gonzalez.fiallos@udla.edu.ec>
2023-10-16 22:15:12 -0500

    paso2

--- ejer11.c
+++ ejer11.c
@@ -4,5 +4,12 @@ int main() {
     printf("Introduce un número entero de dos dígitos: ");
     scanf("%d", &num);
     
+    if (num < 10 || num > 99) {
+        printf("El número no es de dos dígitos.\n");
+
+
+
+
+
     return 0;
 }
