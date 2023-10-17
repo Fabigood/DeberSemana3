1b60a570aaf1b55da789f90fccc15fa344700eb8
Fabigood <fabio.gonzalez.fiallos@udla.edu.ec>
2023-10-16 22:43:11 -0500

    cambiamos a  + el num y 0 si el primer es 0

--- 29ejer.c
+++ 29ejer.c
@@ -5,6 +5,14 @@ int main() {
 
     printf("Introduce un número entero: ");
     scanf("%d", &num);
-    
+      if (num < 0) {
+        num = -num;
+    }
+    if (num == 0) {
+        printf("El primer dígito es igual a 0.\n");
+        }
+
+
+
     return 0;
 }
